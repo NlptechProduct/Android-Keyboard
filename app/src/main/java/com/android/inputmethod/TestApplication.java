769267a191733cad4240381d8766c2052aa1f59d
xuetieheng <tieheng.xue@nlptech.com>
2019-07-30 15:54:17 +0800

    新增theme

--- app/src/main/java/com/android/inputmethod/TestApplication.java
+++ app/src/main/java/com/android/inputmethod/TestApplication.java
@@ -19,15 +19,21 @@ public class TestApplication extends MultiDexApplication {
     public void onCreate() {
         super.onCreate();
         Agent.getInstance().init(this);
-//        addExternalTheme();
-//        Agent.getInstance().loadTheme(this,"001");
+        addExternalTheme();
+        Agent.getInstance().loadTheme(this,"001");
     }
 
     private void addExternalTheme() {
 
-        Drawable keyboardBackgroundDrawable = ContextCompat.getDrawable(this, R.drawable.test_theme_key_press);
+        Drawable keyboardBackgroundDrawable = ContextCompat.getDrawable(this, R.drawable.test_background_lxx_light);
         Drawable morekeysBackgroundDrawable = ContextCompat.getDrawable(this, R.drawable.test_theme_more_keyboard_background);
-        Drawable keyPreviewDrwable = ContextCompat.getDrawable(this, R.drawable.test_theme_key_press);
+        Drawable keyPreviewDrwable = ContextCompat.getDrawable(this, R.drawable.test_theme_preview);
+        Drawable emojiIcon = ContextCompat.getDrawable(this, R.drawable.test_icon_smiley);
+        Drawable deleteIcon = ContextCompat.getDrawable(this, R.drawable.test_icon_delete);
+        Drawable shiftIcon = ContextCompat.getDrawable(this, R.drawable.test_icon_shift);
+        Drawable shiftLockIcon = ContextCompat.getDrawable(this, R.drawable.test_icon_shift_locked);
+        Drawable enterIcon = ContextCompat.getDrawable(this, R.drawable.test_icon_enter);
+        Drawable languageIcon = ContextCompat.getDrawable(this, R.drawable.test_icon_language);
 
         StateListDrawable keyBackgroundDrawable = new StateListDrawable();
         keyBackgroundDrawable.addState(new int[]{android.R.attr.state_pressed}, ContextCompat.getDrawable(this, R.drawable.test_theme_key_press));
@@ -37,10 +43,15 @@ public class TestApplication extends MultiDexApplication {
         functionKeyBackgroundDrawable.addState(new int[]{android.R.attr.state_pressed}, ContextCompat.getDrawable(this, R.drawable.test_theme_function_key_press));
         functionKeyBackgroundDrawable.addState(new int[]{}, ContextCompat.getDrawable(this, R.drawable.test_theme_function_key_normal));
 
+        StateListDrawable morekeyDrawable = new StateListDrawable();
+        functionKeyBackgroundDrawable.addState(new int[]{android.R.attr.state_pressed}, ContextCompat.getDrawable(this, R.drawable.test_theme_more_key_press));
+        functionKeyBackgroundDrawable.addState(new int[]{}, ContextCompat.getDrawable(this, R.drawable.test_theme_key_normal));
+
         StateListDrawable spacebarBackgroundDrawable = new StateListDrawable();
         spacebarBackgroundDrawable.addState(new int[]{android.R.attr.state_pressed}, ContextCompat.getDrawable(this, R.drawable.test_theme_space_key_press));
         spacebarBackgroundDrawable.addState(new int[]{}, ContextCompat.getDrawable(this, R.drawable.test_theme_space_key_normal));
 
+        String color = String.format("#%06X", 0xFFFFFF & 0x12f0e5);
         ExternalThemeInfo externalThemeInfo = new ExternalThemeInfo.Builder("001", "Test Theme")
                 .setKeyboardBackground(keyboardBackgroundDrawable)
                 .setKeyBackground(keyBackgroundDrawable)
@@ -48,11 +59,21 @@ public class TestApplication extends MultiDexApplication {
                 .setSpacebarBackground(spacebarBackgroundDrawable)
                 .setKeyPreviewBackground(keyPreviewDrwable)
                 .setKeyLetterSizeRatio(0.4f)
-                .setKeyTextColor(Color.RED)
-                .setKeyHintLetterColor(Color.BLUE)
-                .setFunctionKeyTextColor(Color.YELLOW)
+                .setKeyTextColor(color)
+                .setKeyHintLetterColor(color)
+                .setFunctionKeyTextColor(color)
+                .setMoreKeysKeyBackground(morekeyDrawable)
                 .setMoreKeysKeyboardBackground(morekeysBackgroundDrawable)
-                .setGestureTrailColor(Color.BLACK)
+                .setGestureTrailColor(color)
+                .setEmojiNormalKeyIcon(emojiIcon)
+                .setEmojiActionKeyIcon(emojiIcon)
+                .setDeleteKeyIcon(deleteIcon)
+                .setShiftKeyIcon(shiftIcon)
+                .setShiftKeyShiftedIcon(shiftLockIcon)
+                .setEnterKeyIcon(enterIcon)
+                .setLanguageSwitchKeyIcon(languageIcon)
+                .setEmojiCategoryPageIndicatorBackgroundColor(color)
+                .setLanguageOnSpacebarTextColor(color)
                 .build();
         Agent.getInstance().addExternalThemes(this, externalThemeInfo);
     }
